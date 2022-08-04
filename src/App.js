2c521f50be754654c184857042f29a609e23b5ad
furkan <furkanesen35@gmail.com>
2022-08-04 18:06:20 +0200

    AppRouter started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppRouter from './router/AppRouter';
 
 const App = () => {
  return (
   <>
-   <p>Movie App</p> 
+   <AppRouter/>
   </>
  );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from "../pages/Main"
+import Register from "../pages/Register"
+import Login from "../pages/Login"
+import MoviesDetail from "../pages/MovieDetail"
 
 const AppRouter = () => {
-  return (
-    <div>
-      <p>Router</p>
-    </div>
-  );
+ return (
+  <BrowserRouter>
+   <Routes>
+    <Route path='/' element={ <Main/> } />
+    <Route path='/login' element={ <Login/> } />
+    <Route path='/register' element={ <Register/> } />
+    <Route path='/detail/:id' element={ <MoviesDetail/> } />
+   </Routes>
+  </BrowserRouter>
+ );
 };
 
 export default AppRouter;
