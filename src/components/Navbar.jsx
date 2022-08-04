6ca94ee6d8831e45359aaf2fbd844c5e1cbb2a49
furkan <furkanesen35@gmail.com>
2022-08-04 18:15:33 +0200

    Navbar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
-  return (
-    <div>
-      <p>Navbar</p>
+ const currentUser = {displayName:"felix franko"}
+ return (
+  <div>
+   <nav className="navbar navbar-extend-lg">
+    <div className="container-fluid">
+     <Link to={"/"} className="navbar-brand text-white" />
+     <h4>React Movie App</h4>
+     <div className="d-flex text-white align-items-center">
+
+     </div>
     </div>
-  );
+   </nav>
+  </div>
+ );
 };
 
 export default Navbar;
