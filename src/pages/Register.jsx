58c7119856ec7a94aa2ee290aa174419207a085f
furkan <furkanesen35@gmail.com>
2022-08-04 19:27:37 +0200

    register page done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Register = () => {
-  return (
-    <div className='d-flex justify-content-center' >
-      <p></p>
-    </div>
-  );
+ const [firstName, setFirstName] = useState()
+ const [lastname, setLastname] = useState()
+ const [email, setEmail] = useState()
+ const [password, setPassword] = useState()
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(firstName,lastname,email,password);
+ }
+ return (
+  <div className='d-flex justify-content-center' >
+   <div className="form-image d-none d-md-block">
+    <img src="https://picsum.photos/800/800" alt="sample-movie" />
+   </div>
+   <div className='register-form'>
+    <h1 className='form-title display-3 '>Register</h1>
+    <form id='register' onSubmit={handleSubmit}>
+     <div className="mb-3">
+      <label htmlFor="firstName" className='form-label'>First Name</label>
+      <input 
+        type="text" className="form-control" id='firstName' placeholder='Enter your first name...' required
+        onChange={(e)=>setFirstName(e.target.value)}
+        />
+      <label htmlFor="lastName" className='form-label'>Last Name</label>
+      <input type="text" className="form-control" id='lastName' placeholder='Enter your last name...' required
+        onChange={(e)=>setLastname(e.target.value)}
+      />
+      <label htmlFor="email" className='form-label'>Email</label>
+      <input type="text" className="form-control" id='email' placeholder='Enter your email...' required
+        onChange={(e)=>setEmail(e.target.value)}
+      />
+      <label htmlFor="password" className='form-label'>Password</label>
+      <input type="password" className="form-control" id='password' placeholder='Enter your password...' required
+        onChange={(e)=>setPassword(e.target.value)}
+      />
+     </div>
+     <input type="submit" className='btn btn-primary form-control' value="Register"/>
+    </form>
+   </div>
+  </div>
+ );
 };
 
 export default Register;
