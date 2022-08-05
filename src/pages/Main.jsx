4ce6e81255973bddce7d91d94b447395ffeb9fa2
furkan <furkanesen35@gmail.com>
2022-08-05 15:42:54 +0200

    loading screen added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useEffect,useState} from 'react';
 
 const API_KEY = "df17deb51dc339d3023e7f1807e2402e";
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
 
 const Main = () => {
-  return (
-    <div>
-      <p>Main</p>
-    </div>
-  );
+ const [movies, setMovies] = useState([])
+ const [loading, setLoading] = useState(false)
+
+ useEffect(() => {
+  getMovies(FEATURED_API)
+ }, [])
+ 
+ const getMovies = (API) => {
+  setLoading(true)
+  axios
+   .get(API)
+   .then((res) => console.log(res.data.results))
+   .catch((err) => console.log(err))
+   .finally(()=>setLoading(false))
+ }
+ return (
+  <div className='d-flex justify-content-center flex-warp' >
+   {loading ? (<div className="spinner-border text-primary m-5" role="status">
+    <span className='sr-only'>Loading...</span>
+   </div>
+   ) : (
+    movies?.map(movie => null)
+    )}
+  </div>
+ );
 };
 
 export default Main;
